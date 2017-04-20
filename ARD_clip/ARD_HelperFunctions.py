88d83a09cf671367ae56ad468918fbacc0c588c8
Matthew Cast <matthew.cast.ctr@usgs.gov>
2017-04-20 09:18:12 -0500

    Replaced logic for reading the bit packed pixelqa file.

--- ARD_clip/ARD_HelperFunctions.py
+++ ARD_clip/ARD_HelperFunctions.py
@@ -113,8 +113,6 @@ def makeMetadataString(metaName):
 #                     each of the various categories.  These counts will be used for calculating
 #                     the % cloud cover, % snow/ice, etc... that will be shown in EarthExplorer.
 #
-#                     5000p X 5000p = 25000000 pixels should all be accounted for.
-#
 def parseHistFile(histFilename):
 
     if (not os.path.isfile(histFilename)):
@@ -130,6 +128,8 @@ def parseHistFile(histFilename):
     bucketsLoc = histLines.find('buckets from')
     colonLoc = histLines.find(':', bucketsLoc + 1)
 
+                                                                # Create an array with the number of occurrences
+                                                                # of all 256 values
     histArray = []
     headLoc = histLines.find('  ', colonLoc) + 2
     while (len(histArray) <= 255):
@@ -137,30 +137,53 @@ def parseHistFile(histFilename):
         histArray.append(histLines[headLoc:tailLoc])
         headLoc = tailLoc + 1
     
-    countFill = long(histArray[1])
-    countClear = long(histArray[2]) + long(histArray[66]) + long(histArray[130]) + long(histArray[194])
-    countWater = long(histArray[4]) + long(histArray[68]) + long(histArray[132]) + long(histArray[196])
-    countShadow = long(histArray[8]) + long(histArray[72]) + long(histArray[136]) + long(histArray[200])
-    countSnow = long(histArray[16]) + long(histArray[80]) + long(histArray[144]) + long(histArray[208])
-    countCloud = long(histArray[32]) + long(histArray[96]) + long(histArray[160]) +long(histArray[224])
+    countClear = 0
+    countWater = 0
+    countSnow = 0
+    countShadow = 0
+    countCloud = 0
+    countFill = 0
     
-                                                                       # Per discussion with Calli Jenkerson and Steve Foga on
-                                                                       # 16 Feb 2017:
-                                                                       #
-                                                                       # The following are Snow/Cloud categories
-                                                                       # They will be treated as "Cloud" for the purposes
-                                                                       # of calculating the percentages
-                                                                       #
+    binNumber = 1
+    while (binNumber <= 255):
+        if (long(histArray[binNumber]) > 0):
 
-    countCombo = long(histArray[48]) + long(histArray[112]) + long(histArray[176]) + long(histArray[240])
-     
-    total = countFill + countClear + countWater + countSnow + countShadow + countCloud + countCombo
-    
-                                                                       # Even if error, send back a tuple 
-    if (total != 25000000):
-        return ('Pixel QA values do not add up',)
-    else:
-        return (countFill, countClear, countWater, countSnow, countShadow, countCloud, countCombo)
+            #print 'bin #=' + str(binNumber) + '  histArray[binNumber] = ' + str(histArray[binNumber])
+            binStart = binNumber
+                                                                # ignore cloud confidence bit
+            if (binStart >= 128):
+                binStart = binStart - 128
+                                                                # ignore cloud confidence bit
+            if (binStart >= 64):
+                binStart = binStart - 64
+
+            if (binStart >= 32):
+                countCloud = countCloud + long(histArray[binNumber])
+                binStart = binStart - 32
+
+            if (binStart >= 16):
+                countSnow = countSnow + long(histArray[binNumber])
+                binStart = binStart - 16
+
+            if (binStart >= 8):
+                countShadow = countShadow + long(histArray[binNumber])
+                binStart = binStart - 8
+
+            if (binStart >= 4):
+                countWater = countWater + long(histArray[binNumber])
+                binStart = binStart - 4
+
+            if (binStart >= 2):
+                countClear = countClear + long(histArray[binNumber])
+                binStart = binStart - 2
+
+            if (binStart >= 1):
+                countFill = countFill + long(histArray[binNumber])
+
+        binNumber = binNumber + 1
+
+    return (countFill, countClear, countWater, countSnow, countShadow, countCloud)
+
 
 # ----------------------------------------------------------------------------------------------
 #
