e45996387a09485a43e804eeb80c27dbdbd3c523
Todd Doerr <todd.doerr.ctr@usgs.gov>
2017-06-05 09:06:32 -0500

    add REGION to view ddl

--- db_tables/ARD_UNPROCESSED_SCENES_V.sql
+++ db_tables/ARD_UNPROCESSED_SCENES_V.sql
@@ -1,4 +1,11 @@
 CREATE or REPLACE VIEW ARD_UNPROCESSED_SCENES_V AS
+select DATE_ACQUIRED, WRS_PATH,WRS_ROW,file_loc,LANDSAT_PRODUCT_ID,satellite,
+       case
+            when WRS_PATH < 49 then 'CONUS'
+            when WRS_PATH||WRS_ROW in (6246,6247,6345,6346,6347,6445,6446,6545,6645) then 'HI'
+            when WRS_PATH||WRS_ROW in (7823,7923,7924,8021,8023,8024,8120,8123,8124,8223,8224,8318,8324,8418,8424,8516,8524,8624,8724,8823,8824,8923,8924,9023) then 'NOGRID'
+            else 'AK'
+       end as REGION from (
 SELECT * FROM
   (
   select
@@ -51,5 +58,6 @@ SELECT * FROM
  LEFT JOIN inventory.lmd_scene d on A.LANDSAT_SCENE_ID = d.landsat_scene_id
  where a.L2_ALBERS_INVENTORY_ID in (select max(L2_ALBERS_INVENTORY_ID) from l2_bridge.l2_albers_inventory group by LANDSAT_SCENE_ID)
  )
- where LANDSAT_PRODUCT_ID in (select scene_id from ARD_PROCESSED_SCENES where PROCESSING_STATE = 'BLANK');
-
+ where LANDSAT_PRODUCT_ID in (select scene_id from ARD_PROCESSED_SCENES where PROCESSING_STATE = 'BLANK')
+)
+/
