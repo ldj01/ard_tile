ac2cecb619c7e3f777e8caec56c3356dbac16b83
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-05-02 11:29:45 -0500

    ENH: generate_tile_id

--- ARD_clip/landsat.py
+++ ARD_clip/landsat.py
@@ -25,3 +25,29 @@ def match_dt(product_id, fields=('acqdate', 'procdate'), dtfmt=r'%Y%m%d'):
     """ Returns results from match(), formatted as datetime """
     return {k: ( datetime.datetime.strptime(v, dtfmt) if k in fields else v )
             for k, v in match(product_id).items()}
+
+
+def generate_tile_id(product_id, current_tile, region, collection, version):
+    """ Create a current Landsat Tile ID based on a product ID
+
+    Args:
+        product_id (str): landsat collection scene product id
+        current_tile (dict): tile information including H/V for grid region
+        region (str): shapefile grid region for provided H/V
+        collection (int): ard landsat processing collection
+        version (int): version of the collection processing this is
+
+    Returns:
+        str: landsat tile id
+
+    Example:
+        >>> generate_tile_id('LE07_L1TP_033037_20130902_20160907_01_A1', {'H': 11, 'V': 3}, 'CU', 1, 1)
+        'LE07_CU_011003_20130902_20200101_C01_V01'
+    """
+    process_date = datetime.datetime.utcnow().strftime('%Y%m%d')
+    product_info = match(product_id)
+    options = dict(today=process_date, region=region, v=version, c=collection)
+    options.update(current_tile)
+    options.update(product_info)
+    tile_id = '{mission}_{region}_{H:03d}{V:03d}_{acqdate}_{today}_C{c:02d}_V{v:02d}'
+    return tile_id.format(**options)
