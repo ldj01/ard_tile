0810a29f5b1c61b4ebd6a358fc528593ac127fab
Lowell Johnson <ldjohn@contractor.usgs.gov>
2018-07-26 11:18:27 -0500

    Revert controller build to local install
    Remove Docker container creation for the controller module.

--- InstallExecutable.cmake
+++ InstallExecutable.cmake
@@ -1,7 +1,7 @@
 ##########################
 # Install an executable as a symbolic link
 
-macro(InstallExecutable _name)
+macro(InstallExecutable _source_dir _name)
 
     if (NOT DEFINED PROJECT_INSTALL_PATH OR
         (DEFINED PROJECT_INSTALL_PATH AND (PROJECT_INSTALL_PATH STREQUAL "")))
@@ -11,18 +11,22 @@ macro(InstallExecutable _name)
 
         set (_bin_path ${PROJECT_INSTALL_PATH}/bin)
 
+        # Install the executable
+        install(PROGRAMS ${_source_dir}/${_name}
+                DESTINATION ${_bin_path})
+
         if (LINK_PROJECT_EXECUTABLES)
             # Install a symbolic link to the executable
             install (
                 CODE "execute_process (
-                         COMMAND ln -sf ${_bin_path}/${_name} ${CMAKE_INSTALL_PREFIX}/bin/${_name}
+                         COMMAND mkdir -p ${CMAKE_INSTALL_PREFIX}/bin
+                     )"
+                CODE "execute_process (
+                         COMMAND ln -sf ${_bin_path}/${_name}
+                                        ${CMAKE_INSTALL_PREFIX}/bin/${_name}
                      )"
             )
         endif ()
-
-        # Install the executable
-        install(PROGRAMS ${_name}
-                DESTINATION ${_bin_path})
 
     endif ()
 
