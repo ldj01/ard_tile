f962fc3f73b1e041802d2ebb4550c90980bd1248
Lowell Johnson <ldjohn@contractor.usgs.gov>
2019-02-28 14:13:33 -0600

    Update version to 1.3.2

--- controller/ardtile.py
+++ controller/ardtile.py
@@ -28,7 +28,7 @@ import cx_Oracle
 
 
 # Specify the software version
-SYSTEM_VERSION = 'ARDTILE_1.3.1'
+SYSTEM_VERSION = 'ARDTILE_1.3.2'
 
 # Specify the ARD tables where we will get info from
 DB_ARD_SCENES = 'ARD_PROCESSED_SCENES'
